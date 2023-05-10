5174a0317d39bb2f9fda5889d257256831824f28
김민지 <minji@gimminjiui-MacBookPro.local>
2023-05-10 13:46:03 +0900

    중간과정

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -10,23 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+//최종적으로 makefile 실행되는지 점검
 #include "ft_printf.h"
+#include <stdio.h>
 
 const char*	ft_printf_s(va_list	ap, const char*	format);
 int		compare_format(const char* format);
 
-int	ft_printf(const char*	format, ...) //printf로 출력하다가 안되면 -1 출력
+int	ft_printf(const char*	format, ...) //리턴값 : 출력 성공 -> 문자열의 길이, 실패 : -1
 {
 	va_list	ap;
+	int		printf_length; //prinft_length -> 수정하기 + 선언만 해둠
 
 	va_start(ap, format);
+	printf_length = 0;
 	while (*format)
 	{
 		if (compare_format(format))
 		{
 			if (*(format + 1) == 's')
 				format = ft_printf_s(ap, format);
-
 		}
 		else
 		{
@@ -35,8 +38,13 @@ int	ft_printf(const char*	format, ...) //printf로 출력하다가 안되면 -1 
 		}
 	}
 	va_end(ap);
-
 	return (0);
+}
+
+int	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
 }
 
 int		compare_format(const char* format)
@@ -66,22 +74,25 @@ int		compare_format(const char* format)
 const char*	ft_printf_s(va_list	ap, const char*	format)
 {
 	char*	str;
-	
+
 	str = va_arg(ap, char*);
 	while (*str)
 	{
-		write(1, &str, 1);
+		write(1, str, 1);
 		str++;
 	}
 	format = format + 2;
 	return (format);
 }
 
-#include <stdio.h>
+///#include <stdio.h>
 int	main(void)
 {
-	ft_printf("name = %s\n", "tom");
-	printf("name = %s\n", "tom");
+	int ret1 = 0, ret2 = 0;
+	ret1 = ft_printf("name = %s\n", "tom");
+	ret2 = printf("name = %s\n", "tom");
+	printf("ret1 : %d\n", ret1);
+	printf("ret2 : %d\n", ret2);
 	//printf(" number : %d\n", 1);
 	return (0);
 }
--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -15,6 +15,7 @@
 
 # include <stdarg.h>
 # include <unistd.h>
+# include <stddef.h>
 
 int		ft_strncmp(const char *str1, const char *str2, size_t n);
 
