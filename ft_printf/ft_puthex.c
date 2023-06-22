bfe1a5e97e425ceecb1757b46d251f378ce17217
김민지 <minji@gimminjiui-MacBookPro.local>
2023-06-22 20:52:52 +0900

    paco -s 수정 완_norm 수정해야 함

--- ft_printf/ft_puthex.c
+++ ft_printf/ft_puthex.c
@@ -1,32 +1,35 @@
 #include "ft_printf.h"
 
-int	ft_puthex(unsigned int num, char c)
+int ft_puthex(unsigned int num, char c)
 {
-	int	length;
+    int length;
 
-	length = 0;
-	length = printf_x((unsigned int)num, &length, c);
-	return (length);
+    length = 0;
+    length = printf_x((unsigned int)num, &length, c);
+    return (length);
 }
 
-int	printf_x(unsigned int n, int	*length, char c)
+int printf_x(unsigned int n, int *length, char c)
 {
-	char	*hex;
+    char *hex;
 
-	if (c == 'x')
-		hex = "0123456789abcdef";
-	else
-		hex = "0123456789ABCDEF";
-	if (n < 16)
-	{
-		write(1, hex + n, 1);
-		(*length)++;
-	}
-	else
-	{
-		printf_x(n / 16, length, c);
-		write(1, hex + (n % 16), 1);
-		(*length)++;
-	}
-	return ((*length));
+    if (c == 'x')
+        hex = "0123456789abcdef";
+    else
+        hex = "0123456789ABCDEF";
+    if (n < 16)
+    {
+        if (write(1, hex + n, 1) == -1)
+            return (-1);
+        (*length)++;
+    }
+    else
+    {
+        if (printf_x(n / 16, length, c) == -1)
+            return (-1);
+        if (write(1, hex + (n % 16), 1) == -1)
+            return (-1);
+        (*length)++;
+    }
+    return (*length);
 }
