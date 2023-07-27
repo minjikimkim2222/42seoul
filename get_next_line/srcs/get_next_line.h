6838640ba1c84d9bd604b184e3fc8d55ae2ad876
김민지 <minji@gimminjiui-MacBookPro.local>
2023-07-28 08:37:59 +0900

    gnl 1차 수정 + norm 수정

--- get_next_line/srcs/get_next_line.h
+++ get_next_line/srcs/get_next_line.h
@@ -2,20 +2,20 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-# define BUFFER_SIZE 100
+#  define BUFFER_SIZE 100
 # endif
 
 # include <stdlib.h>
 # include <unistd.h>
 
-size_t  ft_strlen(char *str);
-char    *ft_strdup(char *s);
-char    *ft_strjoin(char *s1, char *s2);
-char    *ft_strchr(const char *str, int c);
-char    *AppendString(char *backup, char *buf);
-char    *make_line(char *backup);
-char    *update_backup(char *backup);
-char    *read_set_backup(int fd, char *backup);
-char    *get_next_line(int fd);
+size_t	ft_strlen(char *str);
+char	*ft_strdup(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(const char *str, int c);
+char	*append_string(char *backup, char *buf);
+char	*make_line(char *backup);
+char	*update_backup(char *backup);
+char	*read_set_backup(int fd, char *backup);
+char	*get_next_line(int fd);
 
 #endif
